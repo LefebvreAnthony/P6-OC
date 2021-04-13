89c79c4c86939641225004e5d5fcae394dc183f4
Lefebvre Anthony <anthony.lfb@hotmail.fr>
2021-04-13 19:33:55 +0200

    news files BE

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+
+mongoose.connect('mongodb+srv://AnthonyLfb:<kDwpCtT_tGv!v5w>@oc-p6.h4zpy.mongodb.net/myFirstDatabase/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -7,7 +16,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
-  
+
 app.use((req, res, next) => {
  console.log('requête reçue');
  next();
