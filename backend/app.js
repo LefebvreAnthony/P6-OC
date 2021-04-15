2ea1d40620d41254f8de11f62964ff3db3f35f4c
Lefebvre Anthony <anthony.lfb@hotmail.fr>
2021-04-15 19:30:04 +0200

    news routes users

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const bodyParser = require('body-parser');
+const stuffRoutes = require('./routes/stuff');
+
+const userRoutes = require('./routes/user');
 
 
-mongoose.connect('mongodb+srv://AnthonyLfb:<kDwpCtT_tGv!v5w>@oc-p6.h4zpy.mongodb.net/myFirstDatabase/test?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+
+mongoose.connect('mongodb+srv://AnthonyLfb:kDwpCtT_tGv!v5w@oc-p6.h4zpy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+  
+  const app = express();
+app.use(bodyParser.json());
 
-
+app.use('/api/stuff', stuffRoutes);
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+ });
 
-app.use((req, res, next) => {
- console.log('requête reçue');
- next();
-});
-
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
